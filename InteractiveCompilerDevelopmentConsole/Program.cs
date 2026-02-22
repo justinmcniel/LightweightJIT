be219ad49def6d87ec8d8d18c8f57f874f827089
justinmcniel <72999136+justinmcniel@users.noreply.github.com>
2026-02-22 13:33:43 -0600

    ReArranged to support multiple projects in the solution

--- InteractiveCompilerDevelopmentConsole/Program.cs
+++ InteractiveCompilerDevelopmentConsole/Program.cs
@@ -1,3 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using InteractiveCompiler;
 
-Console.WriteLine("Hello, World!");
+namespace InteractiveCompilerDevelopmentConsole
+{
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            string programBody = "";
+            BaseCompiler compiler = new();
+            compiler.RegisterProgram(programBody);
+        }
+    }
+}
